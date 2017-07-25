0216c8d44a9b3d3a98412f6c2ec68fb25cd3111c
Collin James <mail@collinjam.es>
2017-07-25 18:15:55 -0400

    reimplemented with threads

--- projects/prjct1/threadtest.py
+++ projects/prjct1/threadtest.py
@@ -1,14 +1,19 @@
-from multiprocessing import Process, Queue
+# from multiprocessing import Process, Queue
+from threading import Thread
+from Queue import Queue
 import time
-# from sys import stdin
+from sys import stdin
+from select import select
 
-class InputGetter(Process):
+class InputGetter(Thread):
 	"""docstring for InputGetter"""
 	
-	def __init__(self, q):
+	def __init__(self, q, std_in):
 		super(InputGetter, self).__init__()
 		self.q = q
 		self.done = False
+		self.std_in = std_in
+		self.timeout = 0.5
 		
 	def run(self):
 		try:
@@ -17,8 +22,10 @@ class InputGetter(Process):
 			i = ""
 		while i != "owaridayotto":
 			if i != "":
-				# i = std_in.readline()
-				print i
+				rlist, _, _ = select([self.std_in], [], [], self.timeout)
+				if rlist:
+					i = self.std_in.readline()
+					print i
 				# raw_input("hi")
 			else:
 				print "waiting"
@@ -27,17 +34,15 @@ class InputGetter(Process):
 			except Exception, e:
 				i = ""
 
-	def terminate(self):
-		self.done = True
 
 
 if __name__ == '__main__':
 	q = Queue()
 	q.put("hello")
-	ig = InputGetter(q)
+	ig = InputGetter(q, stdin)
 	ig.start()
-	time.sleep(0.5)
+	# time.sleep(0.5)
 	q.put("yikes")
 	q.put("owaridayotto")
 	# ig.terminate()
-	# ig.join()
+	ig.join()
